c656c2e71a0c2c576f8451ca9193710e23f5ccb8
Anshuman202 <asanshuman2@gmail.com>
2023-01-13 21:24:11 +0530

    extending the max method

---
+++ ExtendMax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class testCases<T> where T : IComparable
+    {
+        T a, b, c;
+
+        public T maxi(T a, T b, T c)
+        {
+            if (a.CompareTo(b) == 1 && a.CompareTo(c) == 1)
+                return a;
+            if (b.CompareTo(a) == 1 && b.CompareTo(c) == 1)
+                return b;
+            if (c.CompareTo(a) == 1 && c.CompareTo(b) == 1)
+                return c;
+            return a;
+
+        }
+
+
+    }
+    public class maxy<T> : testCases<T> where T : IComparable
+    {
+        public T maxmathod(T a, T b, T c)
+        {
+            return maxi(a, b, c);
+        }
+    }
+
+}
